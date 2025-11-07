c80584569abdc115e7eda0a671bfc9840987c006
aubert-co <blacket021@gmail.com>
2025-11-07 00:59:40 -0300

    feat: add new TypeScript types and interfaces in src/types/

---
+++ app/backend/src/types/database.types.ts
@@ -0,0 +1,13 @@
+
+export type CountQuery = {
+   count:number
+}
+
+export type SelectLink = {
+   id:number,
+   vacancy_id:number,
+   new_link:string,
+   original_link:string,
+   created_at:string,
+   link_label:string
+}
