ec682e9198c061ac7becbf07e1dd33f33b6153de
aubert-co <blacket021@gmail.com>
2025-11-07 01:01:25 -0300

    chore: add helmet and rate-limit

--- app/backend/src/serve.ts
+++ app/backend/src/serve.ts
@@ -1,11 +1,37 @@
 import express from "express";
+import { addAccessJob } from "./worker/producer";
+import { INVALID_URL, PORTFOLIO, } from './constants'
+import route from "./route/routes";
+import helmet from "helmet";
+import rateLimit from 'express-rate-limit'
 
 const app = express()
 
-app.get('/',(req,res)=>{
-    res.redirect(302,'https://www.google.com.br/index.html')
+app.set('trust proxy',1)
+const globalLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, 
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false, 
+  message: {
+    status: 429,
+    message: "Too many requests from this IP, please try again later."
+  }
+});
+app.use( globalLimiter )
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      useDefaults: true
+    },
+  })
+);
+
+app.use(route)
+
+app.use(async(req,res)=>{
+    await addAccessJob(null,INVALID_URL)
+    res.redirect(302,PORTFOLIO)
 })
 
-app.listen(3000,()=>{
-    console.log('running')
-})
+export default app
