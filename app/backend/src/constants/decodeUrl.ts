41b6f1c015225a90bd56cfdc20d5700df85f3b71
aubert-co <blacket021@gmail.com>
2025-11-07 00:58:12 -0300

    feat: add constants module in src/constants/

---
+++ app/backend/src/constants/decodeUrl.ts
@@ -0,0 +1,40 @@
+import baseX from 'base-x';
+import Hashids from 'hashids';
+
+const BASE62_ALPHABET = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+const base62 = baseX(BASE62_ALPHABET);
+
+const hashid = new Hashids('c3',3)
+export function encodeId(id: number,plataform:string): string {
+    const hashidEncode = hashid.encode(id)
+    const join = hashidEncode + plataform.slice(0,1)
+    const baseEncode = base62.encode(Buffer.from(join)) 
+    
+    return baseEncode
+}
+
+type DecodeObject = {
+    vacancy_id?:string,
+    lastLetter?:string
+}
+export const getLastsLetter = (value:string)=>{
+    const split = value.split('')
+    return split[split.length-1]
+}
+export function decodeId(encoded: string): DecodeObject {
+   try{
+        const decod = base62.decode( encoded )
+ 
+        const decodeed62 = Buffer.from(decod).toString()
+    
+        if(!decodeed62)return {vacancy_id:undefined,lastLetter:undefined}
+        
+        const lastLetter = getLastsLetter(decodeed62)
+        const has = hashid.decode(decodeed62.slice(0,-1))
+        if(has.length === 0)return {vacancy_id:undefined,lastLetter:undefined}
+
+        return {vacancy_id:has[0].toString(),lastLetter}
+   }catch(err){
+    return {vacancy_id:undefined,lastLetter:undefined}
+   }
+}
---
+++ app/backend/src/constants/index.ts
@@ -0,0 +1,11 @@
+export const PORTFOLIO ="https://portfolio.aubertdev.com.br/"
+
+export const NO_CODE  = "no_code"
+
+export const INVALIDE_CODE = "invalide_code"
+
+export const DB_ERROR = "db_error"
+
+export const INVALID_URL = "invalid_url"
+
+export const UNKNOWN ="unknown"
